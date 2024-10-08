0db944f8bd8e0468ce5a457d4e3821b3382e38fa
tamassoltesz <tamas@supertokens.com>
2024-10-08 11:43:15 +0200

    fix: enable previous tests also

--- test.sh
+++ test.sh
@@ -81,107 +81,107 @@ NETWORK_OPTIONS="-p 3567:3567 -e POSTGRESQL_HOST=$POSTGRES_IP"
 NETWORK_OPTIONS_CONNECTION_URI="-p 3567:3567 -e POSTGRESQL_CONNECTION_URI=postgresql://root:root@$POSTGRES_IP:5432"
 printf "\npostgresql_host: \"$POSTGRES_IP\"" >> $PWD/config.yaml
 
-##---------------------------------------------------
-## start with no options
-#docker run -e DISABLE_TELEMETRY=true --rm -d --name supertokens supertokens-postgresql:circleci --no-in-mem-db
-#
-#sleep 10s
-#
-#test_equal `no_of_running_containers` $((no_of_containers_running_at_start+1)) "start with no options"
-#
-##---------------------------------------------------
-## start with no network options, but in mem db
-#docker run -e DISABLE_TELEMETRY=true -p 3567:3567 --rm -d --name supertokens supertokens-postgresql:circleci
-#
-#sleep 17s
-#
-#test_equal `no_of_running_containers` $((no_of_containers_running_at_start+2)) "start with no network options, but in mem db"
-#
-#test_hello "start with no network options, but in mem db"
-#
-#test_session_post "start with no network options, but in mem db"
-#
-#docker rm supertokens -f
-#
-##---------------------------------------------------
-## start with postgresql password
-#docker run -e DISABLE_TELEMETRY=true $NETWORK_OPTIONS -e POSTGRESQL_PASSWORD=root --rm -d --name supertokens supertokens-postgresql:circleci --no-in-mem-db
-#
-#sleep 10s
-#
-#test_equal `no_of_running_containers` $((no_of_containers_running_at_start+1)) "start with postgresql password"
-#
-##---------------------------------------------------
-## start with postgresql user
-#docker run -e DISABLE_TELEMETRY=true $NETWORK_OPTIONS -e POSTGRESQL_USER=root --rm -d --name supertokens supertokens-postgresql:circleci --no-in-mem-db
-#
-#sleep 10s
-#
-#test_equal `no_of_running_containers` $((no_of_containers_running_at_start+1)) "start with postgresql user"
-#
-##---------------------------------------------------
-## start with postgresql user, postgresql password
-#docker run -e DISABLE_TELEMETRY=true $NETWORK_OPTIONS -e POSTGRESQL_USER=root -e POSTGRESQL_PASSWORD=root --rm -d --name supertokens supertokens-postgresql:circleci --no-in-mem-db
-#
-#sleep 17s
-#
-#test_equal `no_of_running_containers` $((no_of_containers_running_at_start+2)) "start with postgresql user, postgresql password"
-#
-#test_hello "start with postgresql user, postgresql password"
-#
-#test_session_post "start with postgresql user, postgresql password"
-#
-#docker rm supertokens -f
-#
-##---------------------------------------------------
-## start with postgresql connection uri
-#docker run -e DISABLE_TELEMETRY=true $NETWORK_OPTIONS_CONNECTION_URI --rm -d --name supertokens supertokens-postgresql:circleci --no-in-mem-db
-#
-#sleep 17s
-#
-#test_equal `no_of_running_containers` $((no_of_containers_running_at_start+2)) "start with postgresql connection uri"
-#
-#test_hello "starstart with postgresql connection uri"
-#
-#test_session_post "start with postgresql connection uri"
-#
-#docker rm supertokens -f
-#
-##---------------------------------------------------
-## start by sharing config.yaml
-#docker run -e DISABLE_TELEMETRY=true $NETWORK_OPTIONS -v $PWD/config.yaml:/usr/lib/supertokens/config.yaml --rm -d --name supertokens supertokens-postgresql:circleci --no-in-mem-db
-#
-#sleep 17s
-#
-#test_equal `no_of_running_containers` $((no_of_containers_running_at_start+2)) "start by sharing config.yaml"
-#
-#test_hello "start by sharing config.yaml"
-#
-#test_session_post "start by sharing config.yaml"
-#
-#docker rm supertokens -f
-#
-## ---------------------------------------------------
-## test info path
-#docker run -e DISABLE_TELEMETRY=true $NETWORK_OPTIONS -v $PWD:/home/supertokens -e POSTGRESQL_USER=root -e POSTGRESQL_PASSWORD=root -e INFO_LOG_PATH=/home/supertokens/info.log -e ERROR_LOG_PATH=/home/supertokens/error.log --rm -d --name supertokens supertokens-postgresql:circleci --no-in-mem-db
-#
-#sleep 17s
-#
-#test_equal `no_of_running_containers` $((no_of_containers_running_at_start+2)) "test info path"
-#
-#test_hello "test info path"
-#
-#test_session_post "test info path"
-#
-#if [[ ! -f $PWD/info.log || ! -f $PWD/error.log ]]
-#then
-#    exit 1
-#fi
-#
-#docker rm supertokens -f
-#
-#rm -rf $PWD/info.log
-#rm -rf $PWD/error.log
+#---------------------------------------------------
+# start with no options
+docker run -e DISABLE_TELEMETRY=true --rm -d --name supertokens supertokens-postgresql:circleci --no-in-mem-db
+
+sleep 10s
+
+test_equal `no_of_running_containers` $((no_of_containers_running_at_start+1)) "start with no options"
+
+#---------------------------------------------------
+# start with no network options, but in mem db
+docker run -e DISABLE_TELEMETRY=true -p 3567:3567 --rm -d --name supertokens supertokens-postgresql:circleci
+
+sleep 17s
+
+test_equal `no_of_running_containers` $((no_of_containers_running_at_start+2)) "start with no network options, but in mem db"
+
+test_hello "start with no network options, but in mem db"
+
+test_session_post "start with no network options, but in mem db"
+
+docker rm supertokens -f
+
+#---------------------------------------------------
+# start with postgresql password
+docker run -e DISABLE_TELEMETRY=true $NETWORK_OPTIONS -e POSTGRESQL_PASSWORD=root --rm -d --name supertokens supertokens-postgresql:circleci --no-in-mem-db
+
+sleep 10s
+
+test_equal `no_of_running_containers` $((no_of_containers_running_at_start+1)) "start with postgresql password"
+
+#---------------------------------------------------
+# start with postgresql user
+docker run -e DISABLE_TELEMETRY=true $NETWORK_OPTIONS -e POSTGRESQL_USER=root --rm -d --name supertokens supertokens-postgresql:circleci --no-in-mem-db
+
+sleep 10s
+
+test_equal `no_of_running_containers` $((no_of_containers_running_at_start+1)) "start with postgresql user"
+
+#---------------------------------------------------
+# start with postgresql user, postgresql password
+docker run -e DISABLE_TELEMETRY=true $NETWORK_OPTIONS -e POSTGRESQL_USER=root -e POSTGRESQL_PASSWORD=root --rm -d --name supertokens supertokens-postgresql:circleci --no-in-mem-db
+
+sleep 17s
+
+test_equal `no_of_running_containers` $((no_of_containers_running_at_start+2)) "start with postgresql user, postgresql password"
+
+test_hello "start with postgresql user, postgresql password"
+
+test_session_post "start with postgresql user, postgresql password"
+
+docker rm supertokens -f
+
+#---------------------------------------------------
+# start with postgresql connection uri
+docker run -e DISABLE_TELEMETRY=true $NETWORK_OPTIONS_CONNECTION_URI --rm -d --name supertokens supertokens-postgresql:circleci --no-in-mem-db
+
+sleep 17s
+
+test_equal `no_of_running_containers` $((no_of_containers_running_at_start+2)) "start with postgresql connection uri"
+
+test_hello "starstart with postgresql connection uri"
+
+test_session_post "start with postgresql connection uri"
+
+docker rm supertokens -f
+
+#---------------------------------------------------
+# start by sharing config.yaml
+docker run -e DISABLE_TELEMETRY=true $NETWORK_OPTIONS -v $PWD/config.yaml:/usr/lib/supertokens/config.yaml --rm -d --name supertokens supertokens-postgresql:circleci --no-in-mem-db
+
+sleep 17s
+
+test_equal `no_of_running_containers` $((no_of_containers_running_at_start+2)) "start by sharing config.yaml"
+
+test_hello "start by sharing config.yaml"
+
+test_session_post "start by sharing config.yaml"
+
+docker rm supertokens -f
+
+# ---------------------------------------------------
+# test info path
+docker run -e DISABLE_TELEMETRY=true $NETWORK_OPTIONS -v $PWD:/home/supertokens -e POSTGRESQL_USER=root -e POSTGRESQL_PASSWORD=root -e INFO_LOG_PATH=/home/supertokens/info.log -e ERROR_LOG_PATH=/home/supertokens/error.log --rm -d --name supertokens supertokens-postgresql:circleci --no-in-mem-db
+
+sleep 17s
+
+test_equal `no_of_running_containers` $((no_of_containers_running_at_start+2)) "test info path"
+
+test_hello "test info path"
+
+test_session_post "test info path"
+
+if [[ ! -f $PWD/info.log || ! -f $PWD/error.log ]]
+then
+    exit 1
+fi
+
+docker rm supertokens -f
+
+rm -rf $PWD/info.log
+rm -rf $PWD/error.log
 git checkout $PWD/config.yaml
 
 #---------------------------------------------------
